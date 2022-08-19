4442deb901a1fa9a188882e403ef9050ee7bb839
AlejandroMT18 <alejandro.redes18@gmail.com>
2022-08-19 10:53:04 -0600

    Fin de la seccion 7

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -24,15 +24,7 @@ export class PokemonService {
       const pokemon = await this.pokemonModel.create(createPokemonDto);
       return pokemon;
     } catch (error) {
-      if (error.code === 11000) {
-        throw new BadRequestException(
-          `Pokemon exist in db ${JSON.stringify(error.keyValue)}`,
-        );
-      }
-      console.log(error);
-      throw new InternalServerErrorException(
-        `Can't create pokemon - Check server logs`,
-      );
+      this.handleException(error);
     }
   }
 
@@ -67,11 +59,42 @@ export class PokemonService {
     return pokemon;
   }
 
-  async update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return await this.pokemonModel.findByIdAndUpdate(id, updatePokemonDto);
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemon = await this.findOne(term);
+    if (updatePokemonDto.name) {
+      updatePokemonDto.name = updatePokemonDto.name.toLocaleLowerCase();
+
+      try {
+        await pokemon.updateOne(updatePokemonDto, { new: true });
+        return { ...pokemon.toJSON(), ...updatePokemonDto };
+      } catch (error) {
+        this.handleException(error);
+      }
+    }
+
+    return pokemon;
   }
 
   async remove(idToSearchAndDelete: string) {
-    return await this.pokemonModel.findByIdAndDelete(idToSearchAndDelete);
+    const { deletedCount } = await this.pokemonModel.deleteOne({
+      _id: idToSearchAndDelete,
+    });
+    if (deletedCount === 0) {
+      throw new BadRequestException(
+        `Pokemon with id "${idToSearchAndDelete}" not found`,
+      );
+    }
+  }
+
+  private handleException(error: any) {
+    if (error.code === 11000) {
+      throw new BadRequestException(
+        `Pokemon exist in db ${JSON.stringify(error.keyValue)}`,
+      );
+    }
+    console.log(error);
+    throw new InternalServerErrorException(
+      `Can't create pokemon - Check server logs`,
+    );
   }
 }
